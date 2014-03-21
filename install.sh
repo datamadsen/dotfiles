f89649778b9c081abb20b41f4aec6c7765e67180
Tim M. Madsen <tim@tmadsen.com>
2014-03-21 22:52:07 +0100

    Remove symlinks before creating and echo stuff.

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-# Replace placeholder in zshrc with completions src dir.
+echo "Replace placeholder in zshrc with completions src dir."
 sed -e "s;{completions_src_dir};$(pwd)/zsh-completions/src;" zshrc > zshrc_with_completions
 
-ln -s $(pwd)/profile ~/.profile
-ln -s $(pwd)/zshrc_with_completions ~/.zshrc
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
+# Remove symlinks
+echo "Remove symlinks:"
+rm ~/.profile   && echo "  ~/.profile"
+rm ~/.zshrc     && echo "  ~/.zshrc"
+rm ~/.tmux.conf && echo "  ~/.tmux.conf"
+
+# Create symlinks
+echo "Create symlinks:"
+ln -s $(pwd)/profile ~/.profile                 && echo "  ~/.profile"
+ln -s $(pwd)/zshrc_with_completions ~/.zshrc    && echo "  ~/.zshrc"
+ln -s $(pwd)/tmux.conf ~/.tmux.conf             && echo "  ~/.tmux.conf"
