7903f539b681e057e80371790513840686890e61
Tim M. Madsen <tim@tmadsen.com>
2014-11-13 22:45:57 +0100

    Remove vundle from bundle; instead, clone via `install.sh`

--- vim/install.sh
+++ vim/install.sh
@@ -1,9 +1,10 @@
-# Remove symlink
 echo "[vim] Remove symlink"
 rm ~/.vim
 
-# Create symlink
 echo "[vim] Create symlink"
 ln -s $(pwd)/ ~/.vim
+
+echo "[vim] Clone vundle"
+git clone https://github.com/gmarik/Vundle.vim.git $(pwd)/bundle/Vundle.vim
 
 echo "[vim] Done - install plugins with \`vim +PluginInstall\`"
