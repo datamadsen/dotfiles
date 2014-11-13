c329802c9dfc0c1aeb244f27c2b62fbb4e92665c
Tim M. Madsen <tim@tmadsen.com>
2014-11-13 19:46:11 +0100

    Remove `zsh_config_dir`; Add zshenv/zlogin

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,20 +2,26 @@
 echo "Zsh"
 
 # Give zsh_config_dig in zshrc a value.
-echo " Search/replace zsh_config_dir placeholder with `pwd`."
-sed -e "s#zsh_config_dir=.*#zsh_config_dir=`pwd`#" zshrc > zshrc.tmp && mv zshrc.tmp zshrc
+#echo "[zsh] Search/replace zsh_config_dir placeholder with `pwd`."
+#sed -e "s#zsh_config_dir=.*#zsh_config_dir=`pwd`#" zshrc > zshrc.tmp && mv zshrc.tmp zshrc
 
 # Make sure that the value in zshrc is not committed.
-echo " Create git filters to not ignore zsh_config_dir value on commit."
-git config filter.zsh_config_dir.smudge 'sed "s#zsh_config_dir=#zsh_config_dir=`pwd`#"'
-git config filter.zsh_config_dir.clean 'sed "s#zsh_config_dir=.*#zsh_config_dir=#"'
+#echo "[zsh] Create git filters to not ignore zsh_config_dir value on commit."
+#git config filter.zsh_config_dir.smudge 'sed "s#zsh_config_dir=#zsh_config_dir=`pwd`#"'
+#git config filter.zsh_config_dir.clean 'sed "s#zsh_config_dir=.*#zsh_config_dir=#"'
 
 # Remove symlink
-echo " Remove symlink."
+echo "[zsh] Remove symlinks"
+rm ~/.zlogin
+rm ~/.zshenv
 rm ~/.zshrc
+rm ~/.git-prompt.sh
 
 # Create symlink
-echo " Create symlink."
+echo "[zsh] Create symlinks."
+ln -s $(pwd)/zlogin ~/.zlogin
+ln -s $(pwd)/zshenv ~/.zshenv
 ln -s $(pwd)/zshrc ~/.zshrc
+ln -s $(pwd)/git-prompt.sh ~/.git-prompt.sh
 
-echo " Done with zsh - remember to source ~/.zshrc"
+echo "[zsh] Done - remember to source ~/.zshrc"
