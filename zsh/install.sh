65bfb01f6c5598819d719e665ffadbee26344f20
Tim M. Madsen <tim@tmadsen.com>
2014-11-13 19:47:56 +0100

    Remove comments

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
-echo "Zsh"
-
-# Give zsh_config_dig in zshrc a value.
-#echo "[zsh] Search/replace zsh_config_dir placeholder with `pwd`."
-#sed -e "s#zsh_config_dir=.*#zsh_config_dir=`pwd`#" zshrc > zshrc.tmp && mv zshrc.tmp zshrc
-
-# Make sure that the value in zshrc is not committed.
-#echo "[zsh] Create git filters to not ignore zsh_config_dir value on commit."
-#git config filter.zsh_config_dir.smudge 'sed "s#zsh_config_dir=#zsh_config_dir=`pwd`#"'
-#git config filter.zsh_config_dir.clean 'sed "s#zsh_config_dir=.*#zsh_config_dir=#"'
 
 # Remove symlink
 echo "[zsh] Remove symlinks"
