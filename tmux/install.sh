6d28719e3eb11351fd424be7121c0cc89e26d3d9
Tim M. Madsen <tim@tmadsen.com>
2015-01-17 22:10:43 +0100

    Add ruby on rails development environment script
    Adds a script that sets up a tmux session with rails for the directory in which
    `devror` is run.
    
    'devror' is an alias that runs that symlinked script that is installed when
    tmux configuration is installed.

--- tmux/install.sh
+++ tmux/install.sh
@@ -5,5 +5,6 @@ rm ~/.tmux.conf
 # Create symlink
 echo "[tmux] Create symlink"
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/dev-env-ruby-on-rails.sh ~/.dev-env-ruby-on-rails.sh
 
 echo "[tmux] Done - remember to source ~/.tmux.conf"
