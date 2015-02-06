734ba1bd847a17f040688df131ba7e9fd300bf67
Tim M. Madsen <tim@tmadsen.com>
2015-02-06 12:38:15 +0100

    Start ssh-agent on each session; reuse socket

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,6 +6,7 @@ rm ~/.zlogin
 rm ~/.zshenv
 rm ~/.zshrc
 rm ~/.git-prompt.sh
+rm ~/.ssh-start.sh
 rm ~/.aliases
 
 # Create symlink
@@ -15,5 +16,6 @@ ln -s $(pwd)/zshenv ~/.zshenv
 ln -s $(pwd)/zshrc ~/.zshrc
 ln -s $(pwd)/git-prompt.sh ~/.git-prompt.sh
 ln -s $(pwd)/aliases ~/.aliases
+ln -s $(pwd)/ssh-start.sh ~/.ssh-start.sh
 
 echo "[zsh] Done - remember to source ~/.zshrc"
