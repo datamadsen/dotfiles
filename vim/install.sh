93155c08c680166869592c0c52e79e8b6a5f6e79
Tim M. Madsen <tim@tmadsen.com>
2015-08-23 11:55:45 +0200

    Modify vim install script

--- vim/install.sh
+++ vim/install.sh
@@ -5,6 +5,6 @@ echo "[vim] Create symlink"
 ln -s $(pwd)/ ~/.vim
 
 echo "[vim] Clone vundle"
-git clone https://github.com/gmarik/Vundle.vim.git $(pwd)/bundle/vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
 
 echo "[vim] Done - install plugins with \`vim +PluginInstall\`"
