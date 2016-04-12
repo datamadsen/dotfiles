24ac3a92c71e41f05df7f34323086ed2e1dc3e45
Tim M. Madsen <tim@tmadsen.com>
2016-04-12 12:41:26 +0200

    Create a notes window

--- tmux/sla-dev.sh
+++ tmux/sla-dev.sh
@@ -7,6 +7,7 @@
 
 SESSION_NAME=sla-dev
 WORKING_DIR=~/source/SLAManagementSystem
+WIKI_DIR=~/vimwiki/sla_management_system
 
 tmux has-session -t $SESSION_NAME 2>/dev/null
 if [ "$?" -eq 1 ] ; then 
@@ -24,6 +25,12 @@ if [ "$?" -eq 1 ] ; then
   tmux send-keys "cd $WORKING_DIR" C-m
   tmux send-keys "clear" C-m
   tmux send-keys "vim +NERDTree" C-m
+
+	# notes
+  tmux new-window -t $SESSION_NAME -n "notes"
+  tmux send-keys "cd $WIKI_DIR" C-m
+  tmux send-keys "clear" C-m
+  tmux send-keys "vim index.wiki" C-m
 
   # select first window
   tmux select-window -t $SESSION_NAME:1
