8165b6b601804d31f9d0b73b12725372e573d818
Tim M. Madsen <tim@tmadsen.com>
2016-05-06 21:50:41 +0200

    Fix ssh-start.sh

--- zsh/ssh-start.sh
+++ zsh/ssh-start.sh
@@ -3,7 +3,7 @@
 export SSH_AUTH_SOCK=~/.ssh-auth-socket
 
 ssh-add -l > /dev/null 2> /dev/null
-if [ $? > 0 ]; then
+if [ $? -ne 0 ]; then
   rm ~/.ssh-auth-socket # otherwise bind addr already in use.
   eval `ssh-agent -s -a ~/.ssh-auth-socket`
   echo "add private key to authentication agent:"
