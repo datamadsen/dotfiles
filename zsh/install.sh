5094abe15419e18663b8664fc3257389ac9c815f
Tim M. Madsen <tim@tmadsen.com>
2018-12-24 10:42:36 +0100

    New prompt with async git information
    Pretty much stole what's here: https://github.com/sindresorhus/pure and
    then modified a bit. For instance the prompt always shows user@host.

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,6 +8,8 @@ rm ~/.zshrc
 rm ~/.git-prompt.sh
 rm ~/.ssh-start.sh
 rm ~/.aliases
+rm /usr/local/share/zsh/site-functions/prompt_pure_setup
+rm /usr/local/share/zsh/site-functions/async
 
 # Create symlink
 echo "[zsh] Create symlinks"
@@ -17,5 +19,7 @@ ln -s $(pwd)/zshrc ~/.zshrc
 ln -s $(pwd)/git-prompt.sh ~/.git-prompt.sh
 ln -s $(pwd)/aliases ~/.aliases
 ln -s $(pwd)/ssh-start.sh ~/.ssh-start.sh
+ln -s $(pwd)/pure.zsh /usr/local/share/zsh/site-functions/prompt_pure_setup
+ln -s $(pwd)/async.zsh /usr/local/share/zsh/site-functions/async
 
 echo "[zsh] Done - remember to source ~/.zshrc"
