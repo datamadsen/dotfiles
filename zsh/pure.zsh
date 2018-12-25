b163c72da06d9809012af0cfaef234abf4d11ee0
Tim M. Madsen <tim@tmadsen.com>
2018-12-25 12:40:40 +0100

    Do not show hostname twice in prompt

--- zsh/pure.zsh
+++ zsh/pure.zsh
@@ -533,9 +533,6 @@ prompt_pure_state_setup() {
 		unset MATCH MBEGIN MEND
 	fi
 
-	# show username@host if logged in through SSH
-	[[ -n $ssh_connection ]] && username='%F{242}%n@%m%f'
-
 	# show username@host if root, with username in white
 	[[ $UID -eq 0 ]] && username='%F{white}%n%f%F{242}@%m%f'
 
