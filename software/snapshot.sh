2c05c9b5ae5c7fd6d9ee61e8816d4f2632c75ec5
Tim M. Madsen <tim@tmadsen.com>
2018-12-29 18:58:59 +0100

    Add more software

--- software/snapshot.sh
+++ software/snapshot.sh
@@ -1,7 +1,7 @@
 echo "brew update..."
 brew update
 brew tap > brew-taps
-brew list -1 > brew-formulae
+brew list -1 > brew-formulas
 brew cask list -1 > brew-casks
-git add brew-taps brew-formulae brew-casks
+git add brew-taps brew-formulas brew-casks
 git diff --cached
