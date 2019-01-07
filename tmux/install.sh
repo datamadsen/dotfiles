2190f25709719d6df10b4ce8c24db4e2cbb8dbe6
Tim M. Madsen <tim@tmadsen.com>
2019-01-07 22:15:08 +0100

    Clone the tpm plugin

--- tmux/install.sh
+++ tmux/install.sh
@@ -8,4 +8,7 @@ echo "[tmux] Create symlinks"
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
 ln -s $(pwd)/ide-rails.sh ~/.ide-rails.sh
 
+echo "[tmux] clone tmux plugin manager"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 echo "[tmux] Done - remember to source ~/.tmux.conf"
