fae51db23fbf53a691abdc0e410fdeae1f237f88
Tim M. Madsen <tim@tmadsen.com>
2019-08-27 12:56:54 +0200

    Tmux color settings dependent on version
    Tmux 2.9 came with breaking changes wrt. styling. Since I still use 2.6
    on some systems and I don't want life to be harder than it has to be I
    make the settings dependent on the version we're using.
    
    \o/

--- utilities/tmux-dark.sh
+++ utilities/tmux-dark.sh
@@ -1,31 +1,65 @@
 #### COLOUR (Solarized dark)
+case `tmux -V` in
+    'tmux 2.9a')
+        # default statusbar colors
+        set-option -g status-style bg=black,fg=white
+        set-option -g status-attr-style default
 
-# default statusbar colors
-set-option -g status-bg black
-set-option -g status-fg white 
-set-option -g status-attr default
+        # default window title colors
+        set-window-option -g window-status-fg-style white
+        set-window-option -g window-status-bg-style black
+        set-window-option -g window-status-attr-style default
 
-# default window title colors
-set-window-option -g window-status-fg white
-set-window-option -g window-status-bg black
-set-window-option -g window-status-attr default
+        # active window title colors
+        set-window-option -g window-status-current-fg-style blue
+        set-window-option -g window-status-current-bg-style default
+        set-window-option -g window-status-current-attr-style default
 
-# active window title colors
-set-window-option -g window-status-current-fg blue
-set-window-option -g window-status-current-bg default
-set-window-option -g window-status-current-attr default
+        # pane border
+        set-option -g pane-border-fg-style black
+        set-option -g pane-active-border-fg-style brightgreen
 
-# pane border
-set-option -g pane-border-fg black 
-set-option -g pane-active-border-fg brightgreen
+        # message text
+        set-option -g message-bg-style black
+        set-option -g message-fg-style brightred
 
-# message text
-set-option -g message-bg black
-set-option -g message-fg brightred
+        # pane number display
+        set-option -g display-panes-active-colour blue
+        set-option -g display-panes-colour brightred
 
-# pane number display
-set-option -g display-panes-active-colour blue
-set-option -g display-panes-colour brightred
+        # clock
+        set-window-option -g clock-mode-colour blue
+    ;;
+    'tmux 2.6')
+        # default statusbar colors
+        set-option -g status-bg black
+        set-option -g status-fg white
+        set-option -g status-attr-style default
 
-# clock
-set-window-option -g clock-mode-colour blue
+        # default window title colors
+        set-window-option -g window-status-fg-style white
+        set-window-option -g window-status-bg-style black
+        set-window-option -g window-status-attr-style default
+
+        # active window title colors
+        set-window-option -g window-status-current-fg-style blue
+        set-window-option -g window-status-current-bg-style default
+        set-window-option -g window-status-current-attr-style default
+
+        # pane border
+        set-option -g pane-border-fg-style black
+        set-option -g pane-active-border-fg-style brightgreen
+
+        # message text
+        set-option -g message-bg-style black
+        set-option -g message-fg-style brightred
+
+        # pane number display
+        set-option -g display-panes-active-colour blue
+        set-option -g display-panes-colour brightred
+
+        # clock
+        set-window-option -g clock-mode-colour blue
+    ;;
+esac
+
