0edbb7dac2a7ddb1d2ebfb4347e4d1b4e5336594
Tim M. Madsen <tim@tmadsen.com>
2019-08-29 15:39:08 +0200

    Commit missing tmux files

---
+++ utilities/new-tmux-dark.sh
@@ -0,0 +1,23 @@
+# default statusbar colors
+set-option -g status-style bg=black,fg=white
+# set-option -g status-attr-style default
+
+# default window title colors
+set-window-option -g window-status-style fg=white,bg=black
+
+# active window title colors
+set-window-option -g window-status-current-style fg=blue,bg=default
+
+# pane border
+set-option -g pane-border-style fg=black
+set-option -g pane-active-border-style fg=brightgreen
+
+# message text
+set-option -g message-style bg=black,fg=brightred
+
+# pane number display
+set-option -g display-panes-active-colour blue
+set-option -g display-panes-colour brightred
+
+# clock
+set-window-option -g clock-mode-colour blue
---
+++ utilities/old-tmux-dark.sh
@@ -0,0 +1,29 @@
+# default statusbar colors
+set-option -g status-bg black
+set-option -g status-fg white
+set-option -g status-attr-style default
+
+# default window title colors
+set-window-option -g window-status-fg-style white
+set-window-option -g window-status-bg-style black
+set-window-option -g window-status-attr-style default
+
+# active window title colors
+set-window-option -g window-status-current-fg-style blue
+set-window-option -g window-status-current-bg-style default
+set-window-option -g window-status-current-attr-style default
+
+# pane border
+set-option -g pane-border-fg-style black
+set-option -g pane-active-border-fg-style brightgreen
+
+# message text
+set-option -g message-bg-style black
+set-option -g message-fg-style brightred
+
+# pane number display
+set-option -g display-panes-active-colour blue
+set-option -g display-panes-colour brightred
+
+# clock
+set-window-option -g clock-mode-colour blue
