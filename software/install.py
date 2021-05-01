5c2a28d29e2cb71daf248e8f92f45f785c34bbfb
Tim M. Madsen <tim@madsen.fm>
2021-05-01 18:19:40 +0200

    Remove casks

--- software/install.py
+++ software/install.py
@@ -8,9 +8,6 @@ def main():
     print("Installing missing formulas")
     install_formulas()
 
-    print("Installing missing casks")
-    install_casks()
-
 def file_lines_to_array(file_name):
     return [line.rstrip('\n') for line in open(file_name)]
 
@@ -29,19 +26,6 @@ def install_formulas():
         print("Installing formula(s): %s" % ', '.join(to_install))
         for formula in to_install:
             subprocess.call("brew install %s" % formula, shell=True)
-
-def install_casks():
-    # Check which, if any formulas to install
-    candidates = file_lines_to_array("brew-casks")
-    currents = shell_cmd_lines_to_array("brew cask list -1");
-    to_install = [cask for cask in candidates if cask not in currents]
-
-    if not to_install:
-        print("Everything is already installed")
-    else:
-        print("Installing casks(s): %s" % ', '.join(to_install))
-        for cask in to_install:
-            subprocess.call("brew cask install %s" % cask, shell=True)
 
 if __name__== "__main__":
   main()
