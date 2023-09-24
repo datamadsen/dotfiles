b3832138ba0512a43eb2cdafb1d70b3c87c2c6e7
Tim M. Madsen <tim@datamadsen.dk>
2023-09-24 09:07:09 +0200

    Reformat

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -1,19 +1,19 @@
 return {
-    {
-        'mfussenegger/nvim-dap',
-        lazy = true,
-        config = require("config.dap"),
-        dependencies = {
-            {
-                'rcarriga/nvim-dap-ui',
-                opts = require("config.dap-ui")
-            },
-            {
-                "nvim-telescope/telescope-dap.nvim",
-                config = function()
-                    require('telescope').load_extension('dap')
-                end
-            }
-        }
+  {
+    'mfussenegger/nvim-dap',
+    lazy = true,
+    config = require("config.dap"),
+    dependencies = {
+      {
+        'rcarriga/nvim-dap-ui',
+        opts = require("config.dap-ui")
+      },
+      {
+        "nvim-telescope/telescope-dap.nvim",
+        config = function()
+          require('telescope').load_extension('dap')
+        end
+      }
     }
+  }
 }
