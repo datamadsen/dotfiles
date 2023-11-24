7df3c48e7ea63ace8c5ec8db58fb004cdf15c077
Tim M. Madsen <tim@datamadsen.dk>
2023-11-24 10:11:34 +0100

    Use ft for finding types

--- nvim/after/plugin/maps.lua
+++ nvim/after/plugin/maps.lua
@@ -33,11 +33,12 @@ leader('/', function()
 end, '[/] Fuzzily search in current buffer')
 
 leader('ff', require('telescope.builtin').find_files, '[F]ind [F]iles')
-leader('fh', require('telescope.builtin').help_tags, '[F]ind [H]elp')
+-- leader('fh', require('telescope.builtin').help_tags, '[F]ind [H]elp')
 leader('fw', require('telescope.builtin').grep_string, '[F]ind current [W]ord')
 leader('fg', require('telescope.builtin').live_grep, '[F]ind by [G]rep')
 leader('fd', require('telescope.builtin').diagnostics, '[F]ind [D]iagnostics')
-leader('ft', '<CMD>Telescope commander<CR>', '[F]ind [T]ims Commands')
+leader('fh', '<CMD>Telescope commander<CR>', '[F]ind [T]ims Commands')
+leader('ft', function () require('telescope.builtin').lsp_dynamic_workspace_symbols({ symbols='class', fname_width=0}) end , '[F]ind [T]ype')
 
 -- Diagnostic keymaps
 normal('[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
