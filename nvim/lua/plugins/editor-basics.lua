7ba0661e074aa5f2c3a2e5fbeb40df8856b1bd42
Tim M. Madsen <tim@datamadsen.dk>
2023-11-24 10:13:37 +0100

    Add scrollbar to nvim

--- nvim/lua/plugins/editor-basics.lua
+++ nvim/lua/plugins/editor-basics.lua
@@ -8,6 +8,7 @@ return {
   { "nvim-pack/nvim-spectre" },
   { 'numToStr/Comment.nvim',   opts = {} },
   { 'folke/which-key.nvim',    opts = {} },
+  { 'petertriho/nvim-scrollbar', opts = {} },
   { 'mbbill/undotree' },
   {
     'nvim-neo-tree/neo-tree.nvim',
