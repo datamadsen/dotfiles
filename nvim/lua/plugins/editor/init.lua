11e637302fd25e0d064b97bc986ef804c0af9c1c
Tim M. Madsen <tim@datamadsen.dk>
2024-02-11 10:11:02 +0100

    Delete unused plugins

--- nvim/lua/plugins/editor/init.lua
+++ nvim/lua/plugins/editor/init.lua
@@ -1,8 +1,6 @@
 return {
   { "tpope/vim-surround" },
   { 'simeji/winresizer' },
-  -- { 'kdheepak/lazygit.nvim' },
-  { 'tpope/vim-fugitive' },
   { 'lewis6991/gitsigns.nvim',   opts = {} },
   { "windwp/nvim-autopairs",     opts = {} },
   { "nvim-pack/nvim-spectre" },
