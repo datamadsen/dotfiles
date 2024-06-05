f1391f015ef825e7650714c88a1232bb77421425
Tim M. Madsen <tim@datamadsen.dk>
2024-06-05 11:04:16 +0200

    copilot changes

--- nvim/lua/plugins/ide/copilot.lua
+++ nvim/lua/plugins/ide/copilot.lua
@@ -1,48 +1,80 @@
+local function on_attach(on_attach)
+	vim.api.nvim_create_autocmd("LspAttach", {
+		callback = function(args)
+			local buffer = args.buf
+			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			on_attach(client, buffer)
+		end,
+  })
+end
+
 return {
-  event = "VeryLazy",
-  cmd = "Copilot",
-  "zbirenbaum/copilot.lua",
-  opts = {
-    panel = {
-      enabled = true,
-      auto_refresh = false,
-      keymap = {
-        jump_prev = "[[",
-        jump_next = "]]",
-        accept = "<CR>",
-        refresh = "gr",
-        open = "<M-CR>"
+  -- copilot
+  {
+    "zbirenbaum/copilot.lua",
+    verylazy = true,
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+        lua = true,
+        bash = true,
+        cs = true,
+        javascript = true,
+        vue = true,
       },
-      layout = {
-        position = "bottom", -- | top | left | right
-        ratio = 0.4
+      server_opts_overrides = {
+        settings = {
+          advanced = {
+            listCount = 10,
+            inlineSuggestionCount = 5
+          },
+        },
+      }
+    },
+  },
+  -- copilot cmp source
+  {
+    "nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        verylazy = true,
+        dependencies = "copilot.lua",
+        opts = {},
+        config = function(_, opts)
+          local copilot_cmp = require("copilot_cmp")
+          copilot_cmp.setup(opts)
+          on_attach(function(client)
+            if client.name == "copilot" then
+              copilot_cmp._on_insert_enter({})
+            end
+          end)
+        end,
       },
     },
-    suggestion = {
-      enabled = true,
-      auto_trigger = false,
-      debounce = 75,
-      keymap = {
-        accept = "<c-l>",
-        accept_word = false,
-        accept_line = false,
-        next = "<c-j>",
-        prev = "<c-k>",
-        dismiss = "<c-h>",
-      },
-    },
-    filetypes = {
-      yaml = false,
-      markdown = false,
-      help = false,
-      gitcommit = false,
-      gitrebase = false,
-      hgcommit = false,
-      svn = false,
-      cvs = false,
-      ["."] = false,
-    },
-    copilot_node_command = 'node', -- Node.js version must be > 18.x
-    server_opts_overrides = {},
-  }
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      table.insert(opts, { sources = { name = "copilot", group_index = 2 } })
+
+      opts.sorting = {
+        priority_weight = 2,
+        comparators = {
+          require("copilot_cmp.comparators").prioritize,
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
+      }
+    end,
+  },
 }
