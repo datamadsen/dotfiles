6cbaf670725c3dd583a0bd04ffffccd3edf47193
Tim M. Madsen <tim@datamadsen.dk>
2024-06-05 22:41:02 +0200

    Add telescope ui select stuff

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,7 +2,15 @@ return {
   {
     -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+    },
   },
   -- Fuzzy Finder Algorithm which requires local dependencies to be built.
   -- Only load if `make` is available. Make sure you have the system
