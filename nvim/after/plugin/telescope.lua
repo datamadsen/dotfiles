f362fe30b03e4fce60a334d46ab222fb54e46362
Tim M. Madsen <tim@datamadsen.dk>
2024-06-05 22:42:46 +0200

    Customize telescope lsp_implementations

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local actions = require('telescope.actions')
+local actions_layout = require('telescope.actions.layout')
 require('telescope').setup {
   defaults = {
     mappings = {
@@ -7,16 +8,60 @@ require('telescope').setup {
         ['<C-d>'] = false,
         ["<esc>"] = actions.close
       },
+      n = {
+        ['p'] = actions_layout.toggle_preview,
+      }
     },
+    layout_strategy = "cursor",
     layout_config = {
       prompt_position = "top",
-      anchor = "NW"
     },
+    sorting_strategy = "ascending",
   },
   pickers = {
     lsp_document_symbols = {
       theme = "dropdown",
       preview = false,
+      on_input_filter_cb = function(prompt)
+        return { prompt = prompt }
+      end
+    },
+    lsp_implementations = {
+      layout_config = {
+        prompt_position = "top",
+        width = 0.9,
+      },
+      initial_mode = "normal",
+      layout_strategy = "cursor",
+      theme = "cursor",
+      title = "implementations",
+      preview = {
+        hide_on_startup = true,
+      },
+      show_line = false,
+      path_display = function(opts, path)
+        local tail = require("telescope.utils").path_tail(path)
+        path = string.format("%s", tail)
+
+        local highlights = {
+          {
+            {
+              0,       -- highlight start position
+              #path,   -- highlight end position
+            },
+            "include", -- highlight group name
+          },
+          {
+            {
+              #path,   -- highlight start position
+              100,     -- highlight end position
+            },
+            "conceal", -- highlight group name
+          },
+        }
+
+        return path, highlights
+      end,
       on_input_filter_cb = function(prompt)
         return { prompt = prompt }
       end
@@ -30,5 +75,13 @@ require('telescope').setup {
         return { prompt = prompt }
       end
     }
+  },
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_cursor {
+        -- even more opts
+      }
+    }
   }
 }
+require("telescope").load_extension("ui-select")
