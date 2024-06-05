9c5e6665c114e7481cb7fa40420d2f7c6256105b
Tim M. Madsen <tim@datamadsen.dk>
2024-06-05 11:06:09 +0200

    New trouble

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -44,9 +44,8 @@ return {
   },
   {
     "folke/trouble.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons"
-    }
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
   },
   -- Eye candy for nvim-lsp progress
   {
