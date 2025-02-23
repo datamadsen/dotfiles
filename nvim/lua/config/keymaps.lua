ca2af57f231c19bd92d7551d00a65f9a9d28bacb
Tim M. Madsen <tim@datamadsen.dk>
2025-02-23 14:58:13 +0100

    WIP commit

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>ac", "<cmd>CodeCompanionChat<cr>", { desc = "Open CodeCompanion Chat" })
+
+vim.keymap.set("n", "<leader>gw", "<cmd>wa | !git wip<cr>", { desc = "WIP commit" })
