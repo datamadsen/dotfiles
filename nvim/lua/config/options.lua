9f6cba190de777bfde1f73c13d163443a8e6702b
Tim M. Madsen <tim@datamadsen.dk>
2025-02-23 10:16:52 +0100

    Try without numbers

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- disable line numers
+vim.opt.number = false
+vim.opt.relativenumber = false
