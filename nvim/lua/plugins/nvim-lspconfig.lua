7261074a99ae72ba90938ee5c44d5aca3e705a01
Tim M. Madsen <tim@datamadsen.dk>
2025-02-23 17:21:07 +0100

    Add instructions for omnisharp for apple silicon

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,6 +3,11 @@ return {
   opts = {
     servers = {
       omnisharp = {
+        -- README:
+        -- If Mason updated omnisharp then it probably selected the wrong architecture.
+        -- see: https://github.com/williamboman/mason.nvim/discussions/1651
+        -- run: MasonInstall --target=darwin_arm64 omnisharp
+
         -- cmd = { "dotnet", "/Users/tmadsen/.cache/omnisharp-osx-arm64-net6.0/OmniSharp.dll", "--languageserver" },
         -- enable_roslyn_analyzers = true,
         -- enable_import_completion = true,
