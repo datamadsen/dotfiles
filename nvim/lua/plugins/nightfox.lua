db0bf0bcb5a706725689901e4af0b2f738948c0b
Tim M. Madsen <tim@datamadsen.dk>
2025-02-23 14:58:33 +0100

    Add nightfox

---
+++ nvim/lua/plugins/nightfox.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "EdenEast/nightfox.nvim",
+    opts = {
+      options = {
+        styles = {
+          comments = "",
+          keywords = "",
+          types = "",
+        },
+      },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nightfox",
+    },
+  },
+}
