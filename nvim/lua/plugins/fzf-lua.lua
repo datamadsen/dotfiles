3cdcf45bb1b96bdfb6dbc1936ec2d24c97db650f
Tim M. Madsen <tim@datamadsen.dk>
2025-02-25 22:23:13 +0100

    Fix this when used from codecompanion

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,16 +1,27 @@
+local function action_noop(_, opts)
+  local o = vim.tbl_deep_extend("keep", { resume = true }, opts.__call_opts)
+  opts.__call_fn(o)
+end
+
 return {
-  {
-    "ibhagwan/fzf-lua",
-    opts = function(_, opts)
-      opts.winopts = {
+  "ibhagwan/fzf-lua",
+  opts = function(_, opts)
+    opts = vim.tbl_deep_extend("force", opts, {
+      actions = {
+        files = {
+          true,
+          ["ctrl-g"] = { action_noop },
+        },
+      },
+      winopts = {
         height = 0.9,
         width = 0.9,
         preview = {
           layout = "vertical",
           vertical = "down:75%",
         },
-      }
-      return opts
-    end,
-  },
+      },
+    })
+    return opts
+  end,
 }
