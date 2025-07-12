9507904815277ed8e4dae73d7eb0a7521b2a3336
Tim M. Madsen <tim@datamadsen.dk>
2025-07-12 23:20:59 +0200

    Add more styles for tokyonight night

--- farv/themes/dark/tokyonight-night/neovim.lua
+++ farv/themes/dark/tokyonight-night/neovim.lua
@@ -1,19 +1,21 @@
 -- Neovim theme configuration for tokyonight-night
 return {
-  "folke/tokyonight.nvim",
-  opts = {
-    day_brightness = 0.1,
-    dim_inactive = true,
-    on_highlights = function(hl, c)
-      local x = "#FFDC00"
-      hl.FlashLabel = {
-        bg = c.yellow,
-        fg = c.black,
-        bold = true,
-      }
-    end,
-  },
-  config = function()
-    vim.cmd("colorscheme tokyonight-night")
-  end,
+	"folke/tokyonight.nvim",
+	opts = {
+		day_brightness = 0.1,
+		dim_inactive = true,
+		on_highlights = function(hl, c)
+			local x = "#FFDC00"
+			hl.FlashLabel = {
+				bg = c.yellow,
+				fg = c.black,
+				bold = true,
+			}
+		end,
+	},
+	config = function()
+		vim.cmd("set background=dark")
+		vim.cmd("colorscheme tokyonight-night")
+	end,
 }
+
