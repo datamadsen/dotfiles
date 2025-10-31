8766beb3b3bf62b0889b782d47b6ab66b27563b2
Tim M. Madsen <tim@datamadsen.dk>
2025-10-31 21:25:55 +0100

    Change tmux installation directory

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,12 +1,14 @@
-# Remove symlink
-echo "[tmux] Remove symlinks"
-rm ~/.tmux.conf
+# Remove old symlinks and directory if it exists
+echo "[tmux] Remove old symlinks and config directory"
+rm -f ~/.tmux.conf
+rm -rf ~/.config/tmux
 
-# Create symlink
-echo "[tmux] Create symlinks"
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
+# Create symlink for entire tmux directory
+echo "[tmux] Create symlink to ~/.config/tmux"
+ln -s $(pwd) ~/.config/tmux
 
 echo "[tmux] clone tmux plugin manager"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+mkdir -p ~/.config/tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
-echo "[tmux] Done - remember to source ~/.tmux.conf"
+echo "[tmux] Done - tmux will automatically load config from ~/.config/tmux/tmux.conf"
