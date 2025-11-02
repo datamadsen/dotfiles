95070addd922d9ae640ef279feb8738ef27e0264
Tim M. Madsen <tim@datamadsen.dk>
2025-11-02 09:38:27 +0100

    Put tpm and plugins in ~/.tmux/plugins/

--- tmux/install.sh
+++ tmux/install.sh
@@ -8,7 +8,7 @@ echo "[tmux] Create symlink to ~/.config/tmux"
 ln -s $(pwd) ~/.config/tmux
 
 echo "[tmux] clone tmux plugin manager"
-mkdir -p ~/.config/tmux/plugins
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "[tmux] Done - tmux will automatically load config from ~/.config/tmux/tmux.conf"
