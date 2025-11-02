fd84f9ac141faf6f89019b5eccfff125ff218b5c
Tim M. Madsen <tim@datamadsen.dk>
2025-11-02 16:05:58 +0100

    Fix the tmux install script

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,14 +1,16 @@
-# Remove old symlinks and directory if it exists
-echo "[tmux] Remove old symlinks and config directory"
-rm -f ~/.tmux.conf
-rm -rf ~/.config/tmux
+# Remove symlink
+echo "[tmux] Remove symlinks"
+rm ~/.tmux.conf
 
-# Create symlink for entire tmux directory
-echo "[tmux] Create symlink to ~/.config/tmux"
-ln -s $(pwd) ~/.config/tmux
+# Create symlink
+echo "[tmux] Create symlinks"
+ln -s "$(pwd)/tmux.conf" ~/.tmux.conf
+mkdir -p ~/.config/tmux
+ln -s "$(pwd)/btop-tmux.sh" ~/.config/tmux/btop-tmux.sh
+ln -s "$(pwd)/btop.conf" ~/.config/tmux/btop.conf
 
 echo "[tmux] clone tmux plugin manager"
-mkdir -p ~/.tmux/plugins
+rm -rf ~/.tmux/plugins/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-echo "[tmux] Done - tmux will automatically load config from ~/.config/tmux/tmux.conf"
+echo "[tmux] Done - remember to source ~/.tmux.conf and install plugins with c-b I"
