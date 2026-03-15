b73de0e7e51cc6cb8377365039a54d705fd1e946
Tim M. Madsen <tim@datamadsen.dk>
2026-03-15 09:36:40 +0100

    Disable spleeing

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,6 +2,14 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Disable spell checking for all filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
+
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.cs",
   callback = function()
